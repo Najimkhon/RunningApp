c683d1ef2923fc70f3f9e21a0ca9e2cc89110891
Najimkhon <qwerty1995.290@gmail.com>
2023-03-28 16:42:56 +0500

    fragments are added

---
+++ app/src/main/java/com/hfad/runningapp/ui/fragments/RunFragment.kt
@@ -0,0 +1,24 @@
+package com.hfad.runningapp.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.hfad.runningapp.R
+import com.hfad.runningapp.ui.viewmodels.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class RunFragment : Fragment() {
+
+    private val viewModel: MainViewModel by viewModels()
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_run, container, false)
+    }
+}
