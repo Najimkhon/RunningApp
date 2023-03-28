e44bf3efac5775e1020dfe37523e5aab098819d0
Najimkhon <qwerty1995.290@gmail.com>
2023-03-28 18:37:03 +0500

    ui layouts and resources are added

--- app/src/main/java/com/hfad/runningapp/ui/fragments/SetupFragment.kt
+++ app/src/main/java/com/hfad/runningapp/ui/fragments/SetupFragment.kt
@@ -5,18 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.hfad.runningapp.R
+import com.hfad.runningapp.base.BaseFragment
+import com.hfad.runningapp.databinding.FragmentSettingsBinding
+import com.hfad.runningapp.databinding.FragmentSetupBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class SetupFragment : Fragment() {
+class SetupFragment : BaseFragment<FragmentSetupBinding>
+    (FragmentSetupBinding::inflate) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setup, container, false)
+    override fun prepareUI() {
+        binding.tvContinue.setOnClickListener{
+            findNavController().navigate(R.id.action_setupFragment_to_runFragment)
+        }
     }
 
 }
