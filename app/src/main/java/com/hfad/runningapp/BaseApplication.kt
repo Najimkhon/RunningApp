17f7848302267f06f528b7a279532f0032bf6f67
Najimkhon <qwerty1995.290@gmail.com>
2023-03-28 15:12:32 +0500

    appModel is added

--- app/src/main/java/com/hfad/runningapp/BaseApplication.kt
+++ app/src/main/java/com/hfad/runningapp/BaseApplication.kt
@@ -1,7 +1,8 @@
 package com.hfad.runningapp
 
+import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class BaseApplication {
+class BaseApplication : Application() {
 }
