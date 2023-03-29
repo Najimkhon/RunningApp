3e84f3ad2055a881b28a07d6b289f5a1ba64b851
Najimkhon <qwerty1995.290@gmail.com>
2023-03-29 12:47:27 +0500

    bottom_nav is fixed

--- app/src/main/java/com/hfad/runningapp/MainActivity.kt
+++ app/src/main/java/com/hfad/runningapp/MainActivity.kt
@@ -3,6 +3,10 @@ package com.hfad.runningapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.hfad.runningapp.databinding.ActivityMainBinding
 import com.hfad.runningapp.db.RunDAO
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -12,10 +16,24 @@ class MainActivity : AppCompatActivity() {
 
     @Inject
     lateinit var runDao: RunDAO
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        navController.addOnDestinationChangedListener{ _, destination, _ ->
+            when(destination.id){
+                R.id.settingsFragment, R.id.runFragment, R.id.statisticsFragment ->
+                    binding.bottomNavigationView.visibility = View.VISIBLE
+                else-> binding.bottomNavigationView.visibility = View.GONE
+            }
+        }
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 }
