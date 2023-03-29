953ad2920fd1e6865947ae3d80682bddaee42f29
Najimkhon <qwerty1995.290@gmail.com>
2023-03-29 15:14:40 +0500

    perissions are handled

--- app/src/main/java/com/hfad/runningapp/ui/fragments/RunFragment.kt
+++ app/src/main/java/com/hfad/runningapp/ui/fragments/RunFragment.kt
@@ -1,24 +1,77 @@
 package com.hfad.runningapp.ui.fragments
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.Manifest
+import android.os.Build
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.hfad.runningapp.R
+import com.hfad.runningapp.base.BaseFragment
+import com.hfad.runningapp.databinding.FragmentRunBinding
 import com.hfad.runningapp.ui.viewmodels.MainViewModel
+import com.hfad.runningapp.utils.Constants.REQUEST_CODE_LOCATION_PERMISSION
+import com.hfad.runningapp.utils.TrackingUtility
 import dagger.hilt.android.AndroidEntryPoint
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
 
 @AndroidEntryPoint
-class RunFragment : Fragment() {
+class RunFragment : BaseFragment<FragmentRunBinding>(FragmentRunBinding::inflate),
+    EasyPermissions.PermissionCallbacks {
 
     private val viewModel: MainViewModel by viewModels()
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_run, container, false)
+
+    override fun prepareUI() {
+       requestPermissions()
+    }
+
+    override fun setListeners() {
+        binding.fab.setOnClickListener{
+            findNavController().navigate(R.id.action_runFragment_to_trackingFragment)
+        }
+    }
+
+    private fun requestPermissions() {
+        if (TrackingUtility.hasLocationPermissions(requireContext())) {
+            return
+        }
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            EasyPermissions.requestPermissions(
+                this,
+                "You need to accept location permissions to use this app.",
+                REQUEST_CODE_LOCATION_PERMISSION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        } else {
+            EasyPermissions.requestPermissions(
+                this,
+                "You need to accept location permissions to use this app.",
+                REQUEST_CODE_LOCATION_PERMISSION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            )
+        }
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            AppSettingsDialog.Builder(this).build().show()
+        } else {
+            requestPermissions()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 }
